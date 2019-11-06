f5bd2d685ae6f78898987a6032c4aded4cd08c1d
joeln <joeln@10.26.146.246>
2019-11-06 15:55:08 -0600

    No crash when close beginning

--- Hackathon/src/client/Paint.java
+++ Hackathon/src/client/Paint.java
@@ -234,50 +234,37 @@ public class Paint extends JPanel implements WindowListener
 	@Override
 	public void windowActivated(WindowEvent e)
 	{
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void windowClosed(WindowEvent e)
 	{
-		System.out.println("Close");
-		System.exit(0);
-
 	}
 
 	@Override
 	public void windowClosing(WindowEvent e)
 	{
-		// TODO Auto-generated method stub
-
+		System.out.println("Closing");
+		System.exit(0);
 	}
 
 	@Override
 	public void windowDeactivated(WindowEvent e)
 	{
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void windowDeiconified(WindowEvent e)
 	{
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void windowIconified(WindowEvent e)
 	{
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void windowOpened(WindowEvent e)
 	{
-		// TODO Auto-generated method stub
-
 	}
 }
