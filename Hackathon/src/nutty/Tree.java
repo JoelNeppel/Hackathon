54b6614fbc4a29553f8e0f8d4866787e5b8d21d0
zgrewell <55713741+zgrewell@users.noreply.github.com>
2019-12-07 16:59:05 -0600

    started updated graphics

--- Hackathon/src/nutty/Tree.java
+++ Hackathon/src/nutty/Tree.java
@@ -2,6 +2,7 @@ package nutty;
 
 import java.awt.Color;
 import java.awt.Graphics;
+//import java.awt.Graphics2D;
 
 public class Tree
 {
@@ -17,12 +18,15 @@ public class Tree
 
 	public static void draw(Graphics g)
 	{
+		//Graphics2D g2d = (Graphics2D)g;
+
 		// Trunk
 		g.setColor(new Color(139, 69, 19));
 		g.fillRect((2 * x) / 5, 0, x / 5, y);
 
 		// Branches
 		g.setColor(new Color(139, 69, 19));
+		
 		for(int i = 0; i < 5; ++i)
 		{
 			g.fillRect(50, 100 + (175 * i), 450, 25);
@@ -31,5 +35,14 @@ public class Tree
 		{
 			g.fillRect(500, 75 + (150 * i), 450, 25);
 		}
+		
+
+		//New Fancy Graphics
+		/*
+		g2d.rotate(Math.PI/6, 500, 500);
+		g.fillRect(50, 100, 450, 25);
+
+		g2d.rotate(-Math.PI/6,500,500);
+		*/
 	}
 }
